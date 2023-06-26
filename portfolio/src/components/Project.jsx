a19836cacc4be4cddb6acb7f1677bb3b5b6142be
Liz <claireliang2012@gmail.com>
2023-06-26 16:50:18 +0200

    Complete portfolio content

--- portfolio/src/components/Project.jsx
+++ portfolio/src/components/Project.jsx
@@ -1,5 +1,6 @@
 //import framer motion
 import { useEffect } from "react";
+import { NavLink } from "react-router-dom";
 import { useAnimation, motion } from "framer-motion";
 import { useInView } from "react-intersection-observer";
 
@@ -10,6 +11,16 @@ import Col from "react-bootstrap/Col";
 
 // import data
 import projectsData from "../data/data.json";
+
+// import icons
+import linkIcon from "../assets/share.png";
+import htmlIcon from "../assets/icons/html5.svg";
+import cssIcon from "../assets/icons/css3-alt.svg";
+import jsIcon from "../assets/icons/square-js.svg";
+import reactIcon from "../assets/icons/react.svg";
+import nodejsIcon from "../assets/icons/node.svg";
+import dbIcon from "../assets/icons/database-solid.svg";
+import gitIcon from "../assets/icons/git-alt.svg";
 
 //for the div variants
 const divVariants = {
@@ -31,6 +42,9 @@ function Project() {
 
   return (
     <div>
+      {/* ------------------- */}
+      {/* --- Project (1) --- */}
+      {/* ------------------- */}
       <motion.div
         key="1"
         ref={ref}
@@ -42,29 +56,51 @@ function Project() {
         <Container className="project">
           <Row>
             <Col xs={12} md={5} className="project-text">
-              <p>Project Name</p>
-              <h3>Office Simulator</h3>
+              <h3 className="project-title">
+                GluttonGlobe{" "}
+                <span>
+                  <NavLink
+                    to="https://bejewelled-alfajores-c72f6e.netlify.app/"
+                    target="_blank"
+                  >
+                    <img src={linkIcon} className="project-link"></img>
+                  </NavLink>
+                </span>
+              </h3>
 
               <hr style={{ border: "#937829 0.5px solid" }}></hr>
               <p>
-                Lorem Ipsum is simply dummy text of the printing and typesetting
-                industry. Lorem Ipsum has been the standard dummy text ever
-                since the 1500s, when an unknown printer took a galley of type
-                and scrambled it to make a type specimen book. It has survived
-                not only five centuries, but also the leap into electronic
+                GluttonGlobe is an interactive world map where users can mark
+                popular cities they have visited or wish to explore. By logging
+                into the app, users can leave their own comments that are
+                visible to others. This app enables a global community of users
+                to discover and share travel recommendations worldwide.
+                Developed in collaboration with partner Panthari Panthong and
+                Solen Wanono.
               </p>
               <hr style={{ border: "#937829 0.5px solid" }}></hr>
               <p>
                 Tech stack:
-                <span></span>
+                <span>
+                  <img src={htmlIcon} className="project-icon"></img>
+                  <img src={cssIcon} className="project-icon"></img>
+                  <img src={jsIcon} className="project-icon"></img>
+                  <img src={reactIcon} className="project-icon"></img>
+                  <img src={nodejsIcon} className="project-icon"></img>
+                  <img src={dbIcon} className="project-icon"></img>
+                  <img src={gitIcon} className="project-icon"></img>
+                </span>
               </p>
             </Col>
             <Col xs={12} md={7} className="project-img">
-              <img src="https://images.unsplash.com/photo-1685276151314-7fe5f99650ee?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80"></img>
+              <img src="https://images.unsplash.com/photo-1516738901171-8eb4fc13bd20?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80"></img>
             </Col>
           </Row>
         </Container>
       </motion.div>
+      {/* ------------------- */}
+      {/* --- Project (2) --- */}
+      {/* ------------------- */}
       <motion.div
         key="2"
         ref={ref}
@@ -76,29 +112,51 @@ function Project() {
         <Container className="project">
           <Row>
             <Col xs={12} md={5} className="project-text">
-              <p>Project Name</p>
-              <h3>Office Simulator</h3>
+              <h3 className="project-title">
+                FitQuest{" "}
+                <span>
+                  <NavLink
+                    to="https://fitness-tracker.adaptable.app/"
+                    target="_blank"
+                  >
+                    <img src={linkIcon} className="project-link"></img>
+                  </NavLink>
+                </span>
+              </h3>
 
               <hr style={{ border: "#937829 0.5px solid" }}></hr>
               <p>
-                Lorem Ipsum is simply dummy text of the printing and typesetting
-                industry. Lorem Ipsum has been the standard dummy text ever
-                since the 1500s, when an unknown printer took a galley of type
-                and scrambled it to make a type specimen book. It has survived
-                not only five centuries, but also the leap into electronic
+                FitQuest is a full CRUD fitness tracker app enabling users to
+                create and track daily workouts. Users log exercises with sets,
+                weights, and reps, earning points based on difficulty. Profile
+                pages allow editing of personal details and display workout
+                history. Styled with a mobile-first approach, it integrates API
+                Ninjas Exercise API for exercise selection and Cloudinary API
+                for profile picture uploads. Developed in collaboration with
+                partner Ian Mason.
               </p>
               <hr style={{ border: "#937829 0.5px solid" }}></hr>
               <p>
                 Tech stack:
-                <span></span>
+                <span>
+                  <img src={htmlIcon} className="project-icon"></img>
+                  <img src={cssIcon} className="project-icon"></img>
+                  <img src={jsIcon} className="project-icon"></img>
+                  <img src={nodejsIcon} className="project-icon"></img>
+                  <img src={dbIcon} className="project-icon"></img>
+                  <img src={gitIcon} className="project-icon"></img>
+                </span>
               </p>
             </Col>
             <Col xs={12} md={7} className="project-img">
-              <img src="https://images.unsplash.com/photo-1685276151314-7fe5f99650ee?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80"></img>
+              <img src="https://images.unsplash.com/photo-1517836357463-d25dfeac3438?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80"></img>
             </Col>
           </Row>
         </Container>
       </motion.div>
+      {/* ------------------- */}
+      {/* --- Project (3) --- */}
+      {/* ------------------- */}
       <motion.div
         key="3"
         ref={ref}
@@ -110,25 +168,40 @@ function Project() {
         <Container className="project">
           <Row>
             <Col xs={12} md={5} className="project-text">
-              <p>Project Name</p>
-              <h3>Office Simulator</h3>
+              <h3 className="project-title">
+                Office Simulator{" "}
+                <span>
+                  <NavLink
+                    to="https://gongtzuuuu.github.io/office-simulator/"
+                    target="_blank"
+                  >
+                    <img src={linkIcon} className="project-link"></img>
+                  </NavLink>
+                </span>
+              </h3>
 
               <hr style={{ border: "#937829 0.5px solid" }}></hr>
               <p>
-                Lorem Ipsum is simply dummy text of the printing and typesetting
-                industry. Lorem Ipsum has been the standard dummy text ever
-                since the 1500s, when an unknown printer took a galley of type
-                and scrambled it to make a type specimen book. It has survived
-                not only five centuries, but also the leap into electronic
+                Developed Office Simulator, a JavaScript-based web game,
+                responsible for implementing precise character control mechanics
+                using the up and down arrow keys. Created an immersive user
+                experience within a simulated office environment, where the main
+                goal is to generate $15 in revenue within a challenging
+                one-minute timeframe.
               </p>
               <hr style={{ border: "#937829 0.5px solid" }}></hr>
               <p>
                 Tech stack:
-                <span></span>
+                <span>
+                  <img src={htmlIcon} className="project-icon"></img>
+                  <img src={cssIcon} className="project-icon"></img>
+                  <img src={jsIcon} className="project-icon"></img>
+                  <img src={gitIcon} className="project-icon"></img>
+                </span>
               </p>
             </Col>
             <Col xs={12} md={7} className="project-img">
-              <img src="https://images.unsplash.com/photo-1685276151314-7fe5f99650ee?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80"></img>
+              <img src="https://images.unsplash.com/photo-1541746972996-4e0b0f43e02a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80"></img>
             </Col>
           </Row>
         </Container>
